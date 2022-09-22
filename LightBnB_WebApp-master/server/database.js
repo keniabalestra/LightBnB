2a5dae89013422dd4f56db73f291a246ca835ea7
Kenia Balestra <kenia.balestra@gmail.com>
2022-09-22 22:41:24 +0000

    Update 'getAllReservations'

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -61,7 +61,7 @@ const addUser = function(user) {
   return pool
     .query(queryString, [user.name, user.email, user.password])
     .then((response) => {
-      //console.log(result.rows)
+      //console.log(response.rows)
       return response.rows[0];
     })
   //return Promise.resolve(user);
@@ -76,7 +76,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`
+  return pool
+  .query(queryString, [guest_id,limit])
+  .then((response) => {
+    //console.log(response.rows)
+    return response.rows;
+  })
 };
 exports.getAllReservations = getAllReservations;
 
